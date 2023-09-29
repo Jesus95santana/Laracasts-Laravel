549a2fa7632b82393d386902aa50d382a01d7590
jesus95santana <jesus95santana@yahoo.com>
2023-09-29 12:25:24 -0400

    2 hours php practice

--- index.php
+++ index.php
@@ -12,8 +12,41 @@
 
     <?php
     $greeting = "Goodbye";
-
+    // Arrary
     $numbers = ['one', 'two', 'three'];
+    // Associative Array
+    $books = [
+            [
+                'name' => 'Dr. Suess',
+                'author' => 'Dr Suess',
+                'link' => 'www.google.com'
+            ],
+            [
+                'name' => 'Dr. Who',
+                'author' => 'Dr Who',
+                'link' => 'www.yahoo.com'
+            ],
+            [
+                'name' => 'Dr. Strange',
+                'author' => 'Dr Strange',
+                'link' => 'www.bing.com'
+            ],
+    ];
+
+    //php version of Array = array_filter($books, function());
+    function filter($items, $key, $value) {
+        $filteredItems = [];
+
+        foreach($items as $item) {
+            if($item[$key] === $value) {
+                $filteredItems[] = $item;
+            }
+        }
+
+        return $filteredItems;
+    }
+
+    $filteredbooks = filter($books, 'name', 'Dr. Suess');
     ?>
 
     <?= "<p>$greeting</p>"?>
@@ -22,5 +55,10 @@
     <li><?= $number ?></li>
     <?php endforeach ?>
 
+    <?php foreach($filteredbooks as $book) :?>
+    <li>Name: <?= $book['name']?></li>
+    <li>Author: <?= $book['author']?></li>
+    <li>URL: <?= $book['link']?></li>
+    <?php endforeach?>
 </body>
 </html>
