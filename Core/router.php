b0df1b7555018aff1edbb1e7871f8dc8bbaf3636
jesus95santana <jesus95santana@yahoo.com>
2023-10-17 09:14:49 -0400

    Insert Namespaces to Classes

--- Core/router.php
+++ Core/router.php
@@ -1,6 +1,6 @@
 <?php
 
-$routes = require('routes.php');
+$routes = require base_path('routes.php');
 
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
@@ -8,7 +8,7 @@ $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
 function routeToController($uri, $routes) {
     if(array_key_exists($uri, $routes)) {
-        require $routes[$uri];
+        require base_path($routes[$uri]);
     } else {
         abort();
     }
@@ -16,7 +16,7 @@ function routeToController($uri, $routes) {
 
 function abort($code = 404) {
     http_response_code($code);
-    require "views/{$code}.php";
+    require base_path("views/{$code}.php");
     die();
 
 }
--- controllers/notes/create.php
+++ controllers/notes/create.php
@@ -1,5 +1,9 @@
 <?php
 
+
+use Core\Database;
+use Core\Validator;
+
 $config = require base_path('config.php');
 $db = new Database($config['database']);
 
