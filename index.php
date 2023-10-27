d3b27eb222af73fe186309711c4a3d0324c0f33e
Jesus Santana <jesus95santana@yahoo.com>
2023-10-27 08:04:53 -0400

    1 hour practice | Classes

--- index.php
+++ index.php
@@ -1,17 +1,45 @@
 <?php
 
-class User
+class Team
 {
-    
-}
+    protected $name;
+    protected $members = [];
 
-class File
-{
-    public $title;
-    public $description;
+    # 'CMD + N' Generate Constructor shortcut (Mac)
+    public function __construct($name, $members = [])
+    {
+        $this->name = $name;
+        $this->members = $members;
+    }
 
-    public function awardTo($user)
+    public static function start($name, $members = [])
+    {
+        return new static($name, $members);
+    }
+
+    public function name()
+    {
+        return $this->name;
+    }
+
+    public function members()
+    {
+        return $this->members;
+    }
+
+    public function add($name)
+    {
+        $this->members[] = $name;
+    }
+
+    public function cancel()
     {
 
     }
 }
+
+$acme = Team::start('Acme',
+    ['John Doe', 'Johnny Sims']);
+
+
+var_dump($acme);
