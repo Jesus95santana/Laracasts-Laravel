ed035f639e3435c8d4717c6d6bf389e469b07d1e
Jesus Santana <jesus95santana@yahoo.com>
2023-11-07 08:23:18 -0500

    Abstract Classes

--- index.php
+++ index.php
@@ -1,21 +1,19 @@
 <?php
 
-class AchievementType
+abstract class AchievementType
 {
-    public function name()
-    {
-
-    }
-
-    public function difficulty()
-    {
-
-    }
-
     public function icon()
     {
-
+        return strtolower(str_replace(' ', '-', $this->name())).'.png';
     }
+
+    public function name()
+    {
+        $class = (new ReflectionClass($this))->getShortName();
+        return trim(preg_replace('/A-Z/', ' $0', $class));
+    }
+
+    abstract public function qualifier($user);
 }
 
 class FirstThousandPoints extends AchievementType
@@ -24,9 +22,12 @@ class FirstThousandPoints extends AchievementType
     {
 
     }
+}
 
-    public function name()
+class ReachTop50 extends AchievementType
+{
+    public function qualifier($user)
     {
-        return 'I overide';
+
     }
 }
