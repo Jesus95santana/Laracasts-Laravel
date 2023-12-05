748f9c21042f5f5e720782c633cbf3977a3ff2ce
Jesus Santana <jesus95santana@yahoo.com>
2023-12-05 08:29:18 -0500

    Introduction to Middleware | Part 1 (10:05)

--- Core/router.php
+++ Core/router.php
@@ -2,47 +2,77 @@
 
 namespace Core;
 
-class Router {
+class Router
+{
     protected $routes = [];
 
-    public function add($method, $uri, $controller) {
-        $this->routes[] = compact('method', 'uri', 'controller');
+    public function get($uri, $controller)
+    {
+        return $this->add('GET', $uri, $controller);
+    }
+
+    public function add($method, $uri, $controller)
+    {
+        $this->routes[] = [
+            'uri' => $uri,
+            'controller' => $controller,
+            'method' => $method,
+            'middleware' => null
+        ];
+        return $this;
     }
 
 //    public function add($method, $uri, $controller)
 //    {
-//        $this->routes[] = [
-//            'uri' => $uri,
-//            'controller' => $controller,
-//            'method' => $method
-//        ];
+//        $this->routes[] = compact('method', 'uri', 'controller');
 //    }
 
-    public function get($uri, $controller) {
-        $this->add('GET', $uri, $controller);
-    }
-    public function post($uri, $controller) {
-        $this->add('POST', $uri, $controller);
-    }
-    public function delete($uri, $controller) {
-        $this->add('DELETE', $uri, $controller);
-    }
-    public function put($uri, $controller) {
-        $this->add('PUT', $uri, $controller);
-    }
-    public function patch($uri, $controller) {
-        $this->add('PATCH', $uri, $controller);
+    public function post($uri, $controller)
+    {
+        return $this->add('POST', $uri, $controller);
     }
 
-    public function route($uri, $method) {
-        foreach($this->routes as $route) {
-            if($route['uri'] === $uri && $route['method'] === strtoupper($method)) {
+    public function delete($uri, $controller)
+    {
+        return $this->add('DELETE', $uri, $controller);
+    }
+
+    public function put($uri, $controller)
+    {
+        return $this->add('PUT', $uri, $controller);
+    }
+
+    public function patch($uri, $controller)
+    {
+        return $this->add('PATCH', $uri, $controller);
+    }
+
+    public function only($key)
+    {
+        $this->routes[array_key_last($this->routes)]['middleware'] = $key;
+        return $this;
+    }
+
+    public function route($uri, $method)
+    {
+        foreach ($this->routes as $route) {
+            if ($route['uri'] === $uri && $route['method'] === strtoupper($method)) {
+                // Middleware
+
+                if ($route['middleware'] === 'guest') {
+                    if ($_SESSION['user'] ?? false) {
+                        header('location: /');
+                        exit();
+                    }
+                }
                 return require base_path($route['controller']);
             }
         }
         $this->abort();
     }
-    protected function abort($code = 404) {
+
+    protected function abort($code = 404)
+    {
         http_response_code($code);
         require base_path("views/{$code}.php");
         die();
--- routes.php
+++ routes.php
@@ -5,7 +5,7 @@ $router->get('/', 'controllers/index.php');
 $router->get('/about', 'controllers/about.php');
 $router->get('/contact', 'controllers/contact.php');
 
-$router->get('/notes', 'controllers/notes/index.php');
+$router->get('/notes', 'controllers/notes/index.php')->only('auth');
 $router->get('/note', 'controllers/notes/note.php');
 $router->delete('/note', 'controllers/notes/delete.php');
 
@@ -15,5 +15,5 @@ $router->patch('/note', 'controllers/notes/update.php');
 $router->post('/notes', 'controllers/notes/store.php');
 $router->get('/notes/create', 'controllers/notes/create.php');
 
-$router->get('/register', 'controllers/registration/create.php');
+$router->get('/register', 'controllers/registration/create.php')->only('guest');
 $router->post('/register', 'controllers/registration/store.php');
